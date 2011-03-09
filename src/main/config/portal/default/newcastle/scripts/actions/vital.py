1d35976e0e8bd7238b9ec510b2f7ccff371adb92
pendlebu <pendlebu@b07a0a23-797b-73b4-9c6d-ec9ec13bac7a>
2011-03-09 01:15:05 +0000

    Refs #1539. Added manual trigger on detail for admins to push data to VITAL.

---
+++ src/main/config/portal/default/newcastle/scripts/actions/vital.py
@@ -0,0 +1,54 @@
+from au.edu.usq.fascinator.common import JsonConfigHelper
+from au.edu.usq.fascinator.common import MessagingServices
+
+class VitalData:
+    def __init__(self):
+        self.messaging = MessagingServices.getInstance()
+
+    def __activate__(self, context):
+        self.auth = context["page"].authentication
+        self.log = context["log"]
+        self.log.info("Manual VITAL integration request received")
+
+        response = context["response"]
+        writer = response.getPrintWriter("text/plain; charset=UTF-8")
+
+        result = JsonConfigHelper()
+        result.set("status", "error")
+        result.set("message", "An unknown error has occurred")
+
+        if self.auth.is_logged_in() and self.auth.is_admin():
+            self.log.info("VITAL: Valid administrative user")
+            formData = context["formData"]
+            func = formData.get("func")
+            oid = formData.get("oid")
+            if func == "refresh":
+                if oid:
+                    self.log.info("VITAL: Refresh event: '{}'", oid)
+                    self.sendMessage(oid);
+                    result.set("status", "ok")
+                    result.set("message", "Object '%s' queued for refresh" % oid)
+                else:
+                    self.log.info("VITAL: No OID provided (500)")
+                    response.setStatus(500)
+                    result.set("message", "No object specified for refresh")
+            else:
+                self.log.info("VITAL: Unknown action '{}' (500)", func)
+                response.setStatus(500)
+                result.set("message", "Unknown action '%s'" % func)
+        else:
+            self.log.info("VITAL: Not an administrative user (500)")
+            response.setStatus(500)
+            result.set("message", "Only administrative users can access this API")
+        writer.println(result.toString(True))
+        writer.close()
+
+    def sendMessage(self, oid):
+        # Fake a workflow reindex. ReDBox doesn't need to reindex,
+        #  the VITAL subscriber just needs to think we did.
+        param = {}
+        param["oid"] = oid
+        param["eventType"] = "ReIndex"
+        param["username"] = self.auth.get_username()
+        param["context"] = "Workflow"
+        self.messaging.onEvent(param)
