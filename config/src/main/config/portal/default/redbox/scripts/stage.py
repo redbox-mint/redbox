8116bb27d2008291cc3903f69915804f4a33ff6e
greg.pendlebury@gmail.com <greg.pendlebury@gmail.com@b07a0a23-797b-73b4-9c6d-ec9ec13bac7a>
2012-05-15 09:14:22 +0000

    Fixes #56. For new sessions, some additional data must be stored when adding a filter.

--- config/src/main/config/portal/default/redbox/scripts/stage.py
+++ config/src/main/config/portal/default/redbox/scripts/stage.py
@@ -6,5 +6,8 @@ class StageData:
         stageLabel = formData.get("stage")
         sessionState.set("fq", 'workflow_step_label:"%s"' % stageLabel)
         sessionState.set("SearchTitle", stageLabel)
+        # Without a session variable storing our portalId, Fascinator will assume
+        # this is a view swap and purge our cached filter before it ever runs.
+        sessionState.set("lastPortalId", context["portalId"])
 
         context["response"].sendRedirect("%s/search" % context["portalPath"])
