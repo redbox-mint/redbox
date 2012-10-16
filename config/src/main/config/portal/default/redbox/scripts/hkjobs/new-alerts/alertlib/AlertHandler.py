6bc27e46f1c56ffb5ad80c3be3ee547e08d4a315
Duncan Dickinson <d.dickinson@qcif.edu.au>
2012-10-16 15:50:02 +1000

    Submitting new alerts code to seperate branch

---
+++ config/src/main/config/portal/default/redbox/scripts/hkjobs/new-alerts/alertlib/AlertHandler.py
@@ -0,0 +1,24 @@
+import os
+
+class AlertHandler(object):
+    """Base handler class for alerts 
+    """
+    def __init__(self, file, config, baseline):
+        """
+        Keyword arguments:
+        file -- The data file being read
+        config -- A com.googlecode.fascinator.common.JsonSimple instance with configuration items
+        baseline -- An object containing a base set of data for the alert
+        
+        """
+        self.file = file
+        if not os.path.exists(self.file):
+            raise AlertException("Requested input file %s does not exist." % self.file)
+        self.config = config
+        self.baseline = baseline
+            
+        
+    def process(self):
+        jsonList = []
+        return jsonList
+    
