c60c47a3a4b9d5962e83c06da24397e81549efa9
Jianfeng Li <li@redboxresearchdata.com.au>
2013-01-25 13:38:04 +1030

    Add UoN mockup reports functions

---
+++ config/src/main/config/portal/default/redbox/scripts/report/ingestReports.py
@@ -0,0 +1,21 @@
+class IngestReportsData:
+
+    def __init__(self):
+        pass
+    def __activate__(self, context):
+        self.auth = context["page"].authentication
+        self.errorMsg = "" 
+        if (self.auth.is_logged_in()):
+            if (self.auth.is_admin()==True):
+                self.buildDashboard(context)
+            else:
+                self.errorMsg = "Requires Admin / Librarian / Reviewer access." 
+        else:
+            self.errorMsg = "Please login."
+        
+    def getErrorMsg(self):
+        return self.errorMsg
+            
+    def buildDashboard(self, context):
+        self.velocityContext = context
+    
---
+++ config/src/main/config/portal/default/redbox/scripts/report/reports.py
@@ -0,0 +1,41 @@
+from java.util import Date
+from java.util import Calendar
+
+class ReportsData:
+
+    def __init__(self):
+        pass
+    def __activate__(self, context):
+        self.auth = context["page"].authentication
+        self.errorMsg = "" 
+        self.request = context["request"]
+        self.fromDtTxt =  self.request.getParameter("from")
+        self.toDtTxt = self.request.getParameter("to")
+        if (self.fromDtTxt is None or self.toDtTxt is None):
+            curCal = Calendar.getInstance()
+            self.fromDtTxt =  "%s-01-01" % curCal.get(Calendar.YEAR)
+            self.toDtTxt =  "%s-12-31" % curCal.get(Calendar.YEAR)
+        if (self.auth.is_logged_in()):
+            if (self.auth.is_admin()==True):
+                self.buildDashboard(context)
+            else:
+                self.errorMsg = "Requires Admin / Librarian / Reviewer access." 
+        else:
+            self.errorMsg = "Please login."
+        
+    def getErrorMsg(self):
+        return self.errorMsg
+            
+    def buildDashboard(self, context):
+        self.velocityContext = context
+               
+    def getFromDt(self):
+        return self.fromDtTxt
+    
+    def getToDt(self):
+        return self.toDtTxt
+    
+    def getDateRange(self):
+        return "from="+ self.getFromDt() + "&to=" + self.getToDt()
+           
+    
