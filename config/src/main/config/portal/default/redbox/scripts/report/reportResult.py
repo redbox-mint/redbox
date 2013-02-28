f5fe48100faff2c89403ec4001b80346d6cb1c7c
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2013-02-28 22:50:26 +1030

    Made changes to Report Result screen as per feedback: 1. Changed Keywords entry to Date Created 2. Added Preview to breadcrumb 3. Modified CSV output so that it only outputs the fields specified in the System-Config.json configuration 4. Also added the ability to change the heading label using the same config as above

--- config/src/main/config/portal/default/redbox/scripts/report/reportResult.py
+++ config/src/main/config/portal/default/redbox/scripts/report/reportResult.py
@@ -4,6 +4,11 @@ from com.googlecode.fascinator.common.solr import SolrResult
 from java.net import URLEncoder
 from java.util import TreeMap
 from org.apache.commons.lang import StringEscapeUtils
+from org.json.simple import JSONArray
+from au.com.bytecode.opencsv import CSVParser
+from au.com.bytecode.opencsv import CSVWriter
+from java.io import StringWriter
+from java.lang import String
 
 class ReportResultData:
 
@@ -18,6 +23,7 @@ class ReportResultData:
         self.reportManager = context["Services"].getService("reportManager")
         self.indexer = context['Services'].getIndexer()
         self.metadata = context["metadata"]
+        self.systemConfig = context["systemConfig"] 
         
         self.errorMsg = "" 
         if (self.auth.is_logged_in()):
@@ -43,24 +49,59 @@ class ReportResultData:
         req.setParam("fq", 'workflow_id:"dataset"')
         if (self.format == "csv"): 
             out = ByteArrayOutputStream()
+            self.fields = self.systemConfig.getArray("redbox-reports","csv-output-fields")
+            
+              
             recnumreq = SearchRequest(self.reportQuery)
             recnumreq.setParam("fq", 'item_type:"object"')
             recnumreq.setParam("fq", 'workflow_id:"dataset"')
+            
             recnumreq.setParam("rows", "0")
             self.indexer.search(recnumreq, out)
             recnumres = SolrResult(ByteArrayInputStream(out.toByteArray()))
             req.setParam("rows", "%s" % recnumres.getNumFound())
             
-            self.out = self.response.getOutputStream("text/csv")
+            if self.fields is not None:
+                fieldString = ""
+                for field in self.fields:
+                  fieldString = fieldString+ field.get("field-name")+","
+                fieldString = fieldString[:-1]
+                req.setParam("fl",fieldString)
+                
+            
+            out = ByteArrayOutputStream()
             self.response.setHeader("Content-Disposition", "attachment; filename=%s.csv" % self.report.getLabel())
-            self.indexer.search(req, self.out, self.format)
-            self.out.close();
+            self.indexer.search(req, out, self.format)
+            csvResponseString = String(out.toByteArray(),"utf-8")
+            csvResponseLines = csvResponseString.split("\n")
+            self.out = self.response.getOutputStream("text/csv")
+            sw = StringWriter()
+            parser = CSVParser()
+            writer = CSVWriter(sw)
+            count = 0
+            for line in csvResponseLines:
+                csvLine = parser.parseLine(line)
+                if count == 0 :
+                    for idx, csvValue in enumerate(csvLine):
+                        csvLine[idx] = self.findDisplayLabel(csvValue)
+                
+                writer.writeNext(csvLine)
+
+            self.out.print(sw.toString())
+            self.out.close()
         else:    
             req.setParam("rows", "1000")
             out = ByteArrayOutputStream()
             self.indexer.search(req, out)
             self.__reportResult = SolrResult(ByteArrayInputStream(out.toByteArray()))
-            
+
+    def findDisplayLabel(self, csvValue):
+        if self.fields is not None:
+            for field in self.fields:
+                if field.get("field-name") == csvValue:
+                    return field.get("label")    
+        return csvValue
+                
     def getErrorMsg(self):
         return self.errorMsg
             
@@ -77,7 +118,8 @@ class ReportResultData:
         return self.report.getLabel()
     
     def urlEncode(self, text):
-        return URLEncoder.encode(text, "utf-8")    
+        return URLEncoder.encode(text, "utf-8")
+        
     def escapeHtml(self, value):
         if value:
             return StringEscapeUtils.escapeHtml(value) or ""
