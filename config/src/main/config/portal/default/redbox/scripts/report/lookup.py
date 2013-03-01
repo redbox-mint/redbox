f0a00500f28c5e2f40af4019cd9c7bfa0c80b40f
andrewbrazzatti <andrew@redboxresearchdata.com.au>
2013-03-01 12:47:12 +1030

    Added more options in the report criteria:

--- config/src/main/config/portal/default/redbox/scripts/report/lookup.py
+++ config/src/main/config/portal/default/redbox/scripts/report/lookup.py
@@ -32,33 +32,106 @@ class LookupData:
         self.term = context["formData"].get("term")
         self.writer = self.response.getPrintWriter("application/json")
         if (self.field=="creator"):
-            out = ByteArrayOutputStream()
-            req = SearchRequest("creatorfullname:%s*" % self.term)
-            req.setParam("fq", 'item_type:"object"')
-            req.setParam("fq", 'workflow_id:"dataset"')
-            req.setParam("rows", "1000")
-            self.indexer.search(req, out)
-            res = SolrResult(ByteArrayInputStream(out.toByteArray()))
-            hits = HashSet()
-            if (res.getNumFound() > 0):
-                creatorResults = res.getResults()
-                for creatorRes in creatorResults:
-                    creatorList = creatorRes.getList("creatorfullname")
-                    if (creatorList.isEmpty()==False):
-                        for hit in creatorList:
-                            hits.add(hit)
-                self.writer.print("[")
-                hitnum = 0
-                for hit in hits:
-                    if (hitnum > 0):
-                        self.writer.print(",\"%s\"" % hit)
-                    else:    
-                        self.writer.print("\"%s\"" % hit)
-                    hitnum += 1
-                self.writer.print("]")
-            else:   
-                 self.writer.println("[\"\"]")
-            self.writer.close()
+            self.handleCreator()
+            return
+        if (self.field=="recordType"):
+            self.handleRecordType()
+            return
+        if (self.field=="workflowStep"):
+#
+            self.handleWorkflowStep()
+            return
 
+    def handleWorkflowStep(self):
+        out = ByteArrayOutputStream()
+        req = SearchRequest("workflow_step_label:[* TO *]" )
+        req.setParam("fq", 'item_type:"object"')
+        req.setParam("fq", 'workflow_id:"dataset"')
+        req.setParam("rows", "1000")
+        self.indexer.search(req, out)
+        res = SolrResult(ByteArrayInputStream(out.toByteArray()))
+        hits = HashSet()
+        if (res.getNumFound() > 0):
+            recordTypeResults = res.getResults()
+            for recordTypeResult in recordTypeResults:
+                recordTypeList = recordTypeResult.getList("workflow_step_label")
+                if (recordTypeList.isEmpty()==False):
+                    for hit in recordTypeList:
+                        hits.add(hit)
+            self.writer.println("[")
+            
+            hitnum = 0
+            for hit in hits:
+                if (hitnum > 0):
+                    self.writer.println(",{\"value\": \"%s\",\n\"label\": \"%s\"}" % (hit,hit))
+                else:    
+                    self.writer.println("{\"value\": \"%s\",\n\"label\": \"%s\"}" % (hit,hit))
+                hitnum += 1
+            self.writer.println("]")
+        else:   
+             self.writer.println("[\"\"]")
+        self.writer.close()
+        
+    def handleRecordType(self):
+        out = ByteArrayOutputStream()
+        req = SearchRequest("dc\:type.skos\:prefLabel\:[* TO *]" )
+        req.setParam("fq", 'item_type:"object"')
+        req.setParam("fq", 'workflow_id:"dataset"')
+        req.setParam("rows", "1000")
+        self.indexer.search(req, out)
+        res = SolrResult(ByteArrayInputStream(out.toByteArray()))
+        hits = HashSet()
+        if (res.getNumFound() > 0):
+            recordTypeResults = res.getResults()
+            for recordTypeResult in recordTypeResults:
+                recordTypeList = recordTypeResult.getList("dc:type.skos:prefLabel")
+                if (recordTypeList.isEmpty()==False):
+                    for hit in recordTypeList:
+                        hits.add(hit)
+            self.writer.println("[")
+            
+            hitnum = 0
+            for hit in hits:
+                if (hitnum > 0):
+                    self.writer.println(",{\"value\": \"%s\",\n\"label\": \"%s\"}" % (hit,hit))
+                else:    
+                    self.writer.println("{\"value\": \"%s\",\n\"label\": \"%s\"}" % (hit,hit))
+                hitnum += 1
+            self.writer.println("]")
+        else:   
+             self.writer.println("[\"\"]")
+        self.writer.close()
+        
+        
+    def handleCreator(self):
+        out = ByteArrayOutputStream()
+        req = SearchRequest("creatorfullname:%s*" % self.term)
+        req.setParam("fq", 'item_type:"object"')
+        req.setParam("fq", 'workflow_id:"dataset"')
+        req.setParam("rows", "1000")
+        self.indexer.search(req, out)
+        res = SolrResult(ByteArrayInputStream(out.toByteArray()))
+        hits = HashSet()
+        if (res.getNumFound() > 0):
+            creatorResults = res.getResults()
+            for creatorRes in creatorResults:
+                creatorList = creatorRes.getList("creatorfullname")
+                if (creatorList.isEmpty()==False):
+                    for hit in creatorList:
+                        hits.add(hit)
+            self.writer.print("[")
+            hitnum = 0
+            for hit in hits:
+                if (hitnum > 0):
+                    self.writer.print(",\"%s\"" % hit)
+                else:    
+                    self.writer.print("\"%s\"" % hit)
+                hitnum += 1
+            self.writer.print("]")
+        else:   
+             self.writer.println("[\"\"]")
+        self.writer.close()
+        
+        
     def getErrorMsg(self):
         return self.errorMsg
