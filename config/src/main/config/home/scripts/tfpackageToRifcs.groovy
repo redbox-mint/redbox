484e1513fb9c5b1b66547fc86ed7d5c527730989
mattRedBox <matt@redboxresearchdata.com.au>
2016-11-03 18:28:53 +1000

    REDBOX-449. corrected package for script.

--- config/src/main/config/home/scripts/tfpackageToRifcs.groovy
+++ config/src/main/config/home/scripts/tfpackageToRifcs.groovy
@@ -17,9 +17,7 @@
  *    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  * /
  */
-
-
-package home.scripts;
+package au.com.redboxresearchdata.rifcs.transformer;
 
 
 import au.com.redboxresearchdata.rifcs.ands.builder.impl.RifcsCollectionBuilder
